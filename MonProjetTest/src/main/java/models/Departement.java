33e9e122f163542ddbb0f750c8fe892a82d90bf9
Etsoa <fetraniaina26@gmail.com>
2025-12-01 09:51:26 +0300

    Add Employe and Departement models, remove Personne
    Introduces new Employe and Departement classes with fields, constructors, getters/setters, and save methods. Removes the Personne class, likely as part of a refactor to replace Personne with more specific models.

---
+++ MonProjetTest/src/main/java/models/Departement.java
@@ -0,0 +1,39 @@
+package models;
+
+public class Departement {
+    private Integer id;
+    private String nom;
+
+    public Departement() {
+    }
+
+    public Departement(Integer id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void save() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Departement{id=" + id + ", nom='" + nom + "'}";
+    }
+}
